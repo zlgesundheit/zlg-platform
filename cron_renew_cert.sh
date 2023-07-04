266fdb3beecd9ffcf31bc6b0a29edb5df5dc1b3c
Jendrik Richter <jendrik.richter@med.uni-goettingen.de>
2023-07-04 10:39:35 +0200

    Added script for cert-renewal via cronjob

---
+++ cron_renew_cert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Change to the directory where your Docker Compose file is located
+cd /home/richter122/zlg-platform
+
+# Start the containers using Docker Compose
+docker-compose up certbot
+docker-compose nginx nginx -s reload
